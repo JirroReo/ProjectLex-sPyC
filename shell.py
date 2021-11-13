0a4d3e26153c0f501bc190bcfd814fbeabaf432f
kathulhur <josephkarl19@outlook.com>
2021-11-13 22:54:06 +0800

    Display on the filename, not the entire path; Added print_tokens function to format console output

--- shell.py
+++ shell.py
@@ -1,26 +1,28 @@
 from Lexer.lexer import Lexer
+from Components.tokens import print_tokens
 from Parser.parser import Parser
 import sys
+import os
+
 
 def analyze_source(fn, text):
     result, error = run(fn, text)
-
     if error: print(error.__str__())
-    else: print(result)
+    else: print_tokens(result)
 
 def run(fn, text):
     # Analyze lexigraph
     lexer = Lexer(fn, text)
     tokens, error = lexer.make_tokens()
     if error: return None, error
-    # return tokens, error
+    return tokens, error
     # ^ [Uncomment if gonna use Lexicographical Analyzer]
 
     # v [Turn block into comment if gonna use Parser]
     # Generate Abstract Syntax Tree 
-    parser = Parser(tokens)
-    ast = parser.parse()
-    return ast.node, ast.error
+    # parser = Parser(tokens)
+    # ast = parser.parse()
+    # return ast.node, ast.error
 
 if __name__ == "__main__":
     if('-f' in sys.argv): # checks if the option f is provided as a command line argument
@@ -28,14 +30,20 @@ if __name__ == "__main__":
         fileOptionArgIdx = fileOptionIdx + 1 # file option argument index
 
         # ensures that an file option argument is given and that it does not start with '-'
-        filename = sys.argv[fileOptionArgIdx] if len(sys.argv) > fileOptionArgIdx and sys.argv[fileOptionArgIdx][0] != '-' else False
-
-        if(filename): #if the filename is not false
+        filepath = sys.argv[fileOptionArgIdx] if len(sys.argv) > fileOptionArgIdx and sys.argv[fileOptionArgIdx][0] != '-' else False
+        
+        if(filepath): #if the filename is not false
+            head, filename = os.path.split(filepath)
             print('filename: ', filename) #output the file name
+            print('\n')
+            print('TOKENS\t\tLEXEMES')
+            print('------------------------')
+
+
             try: # 
-                with open(filename, 'r') as f: #Open the file
+                with open(filepath, 'r') as f: #Open the file
                     text = f.read() # get the whole text in the file
-                    analyze_source(filename, text) # run the lexical and syntax analyzer 
+                    analyze_source(filepath, text) # run the lexical and syntax analyzer 
 
             except FileNotFoundError: #catch the error when FileNotFoundError occurs
                 print('File not found!')
